4bb2ab490baf3cdd609afca70bea020da29def96
ArnoSchiller <arno-schiller@hotmail.de>
2023-06-29 09:51:09 +0200

    Initial project setup

--- packing_utils/data/container.py
+++
@@ -1,5 +0,0 @@
-
-class Container():
-    def __init__(self, width: int, height: int):
-        self.width = width
-        self.height = height
---
+++ tests/test_demo.py
@@ -0,0 +1,9 @@
+from packutils.data.item import test_constant
+
+
+def test_constants():
+    assert test_constant == 'test'
+
+
+if __name__ == '__main__':
+    test_constants()
