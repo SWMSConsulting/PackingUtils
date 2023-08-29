2d5795bc648a6f8561de541e6c5a4d2d280e0bbf
ArnoSchiller <arno-schiller@hotmail.de>
2023-08-29 12:03:09 +0200

    Adapted the endpoint return

--- api.py
+++ api.py
@@ -4,9 +4,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
+from packutils.data.bin import Bin
 from packutils.data.order import Order
 from packutils.data.article import Article
-from packutils.data.bin import Bin
+from packutils.data.packed_order import PackedOrder
 from packutils.solver.palletier_packer import PalletierPacker
 
 app = FastAPI()
@@ -31,7 +32,7 @@ async def status():
     return {"status": "Healthy"}
 
 
-@app.post("/predictions")
+@app.post("/palletier")
 async def get_packing(orderModel: OrderModel):
 
     order = Order(
@@ -49,33 +50,10 @@ async def get_packing(orderModel: OrderModel):
 
     bin = Bin(800, 1, 500)
     solver = PalletierPacker(bins=[bin])
-
     variant = solver.pack_variant(order)
-    print(variant)
 
-    positions = [
-        {
-            'article_id': pack.id,
-            'x': pack.position.x, 'y': pack.position.y, 'z': pack.position.z,
-            'rotation': pack.position.rotation,
-            'centerpoint_x': pack.centerpoint().x,
-            'centerpoint_y': pack.centerpoint().y,
-            'centerpoint_z': pack.centerpoint().z
-        }
-        for pack in variant.bins[0].packed_items
-    ] if len(variant.bins) > 0 else []
-    colli = {'colli': 1, 'colli_total': 1,
-             'colli_dimension': {
-                 "width": bin.width,
-                 "length": bin.length,
-                 "height": bin.height
-             },
-             'positions': positions}
+    packed_order = PackedOrder(order_id=order.order_id)
+    if variant != None:
+        packed_order.add_variant(variant)
 
-    packed = {
-        "order_id": order.order_id,
-        "packing_variants": [[colli]],
-        "articles": orderModel.articles,
-    }
-
-    return packed
+    return packed_order.to_json()
