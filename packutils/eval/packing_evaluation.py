e91de1841e5ffc0b7484161cfeb17dae43fee4f4
ArnoSchiller <arno-schiller@hotmail.de>
2023-09-18 12:21:17 +0200

    Tested and added scoring functions

--- packutils/eval/packing_evaluation.py
+++ packutils/eval/packing_evaluation.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 from packutils.data.bin import Bin
+from packutils.data.item import Item
 from packutils.data.packing_variant import PackingVariant
+
 
 class PackingEvaluationWeights():
     def __init__(
         self,
-        item_distribution: float = 0.0
+        item_distribution: float = 0.0,
+        item_stacking: float = 0.0,
+        item_grouping: float = 0.0
     ):
         self.item_distribution = item_distribution
-        
-        
+        self.item_stacking = item_stacking
+        self.item_grouping = item_grouping
+
 
 class PackingEvaluation():
     def __init__(self, weights: PackingEvaluationWeights):
@@ -18,14 +23,24 @@ class PackingEvaluation():
 
     def evaluate_packing_variant(self, variant: PackingVariant):
         score = np.mean([self.evaluate_bin(bin) for bin in variant.bins])
-        return score 
+        return score
 
-    
     def evaluate_bin(self, bin: Bin):
         score = 0
-        
+
         if self.weights.item_distribution != 0:
-            score += self.weights.item_distribution * self._evaluate_item_distribution(bin)
+            score += self.weights.item_distribution * \
+                self._evaluate_item_distribution(bin)
+
+        if self.weights.item_stacking != 0:
+            score += self.weights.item_stacking * \
+                self._evaluate_item_stacking(bin)
+
+        if self.weights.item_grouping != 0:
+            score += self.weights.item_grouping * \
+                self._evaluate_item_grouping(bin)
+
+        return score
 
     def _evaluate_item_distribution(self, bin: Bin):
         """
@@ -36,9 +51,62 @@ class PackingEvaluation():
         scores = []
 
         for item in bin.packed_items:
-            rel_distance_center =  abs(item.centerpoint().x - bin.width / 2) / (bin.width / 2)
-            rel_volume = item.volume / bin.get_used_volume(use_percentage=False)
+            rel_distance_center = abs(
+                item.centerpoint().x - bin.width / 2) / (bin.width / 2)
+            rel_volume = item.volume / \
+                bin.get_used_volume(use_percentage=False)
             scores.append(rel_distance_center * rel_volume)
 
-        print(scores)
+        return np.mean(scores)
+
+    def _evaluate_item_stacking(self, bin: Bin):
+        """
+        The goal is to put smaller items on top of larger items. Therefore the distance 
+        """
+
+        scores = []
+        for item in bin.packed_items:
+            rel_distance_bottom = item.position.z / bin.height
+            rel_volume = item.volume / \
+                bin.get_used_volume(use_percentage=False)
+            scores.append(rel_distance_bottom / rel_volume)
+
+        return np.mean(scores)
+
+    def _distance_between_items(self, item1: Item, item2: Item):
+        p1 = np.array(
+            [item1.centerpoint().x, item1.centerpoint().y, item1.centerpoint().z])
+        p2 = np.array(
+            [item2.centerpoint().x, item2.centerpoint().y, item2.centerpoint().z])
+        squared_dist = np.sum((p1-p2)**2, axis=0)
+        distance = np.sqrt(squared_dist)
+
+        return distance
+
+    def _evaluate_item_grouping(self, bin: Bin):
+        """
+        The goal is to group items of same type. The score is calculated by counting touching items (direct neighbors). This number is divided by the number of other items in the group or by 4 if more than 5 items are in the group. 
+        """
+
+        scores = []
+
+        groups = set([(item.width, item.length, item.height)
+                     for item in bin.packed_items])
+
+        for group in groups:
+            group_items = [item for item in bin.packed_items if (
+                item.width, item.length, item.height) == group]
+
+            group_scores = []
+
+            for item in group_items:
+                touching_items = [other for other in group_items if (
+                    abs(item.position.x - other.position.x) == item.width or
+                    abs(item.position.y - other.position.y) == item.length or
+                    abs(item.position.z - other.position.z) == item.height
+                )]
+                group_scores.append(len(touching_items) /
+                                    min(max(len(group_items)-1, 1), 4))
+
+            scores.append(np.mean(group_scores))
         return np.mean(scores)
--- tests/eval/test_packing_evaluation.py
+++ tests/eval/test_packing_evaluation.py
@@ -4,97 +4,74 @@ from packutils.data.bin import Bin
 from packutils.data.item import Item
 from packutils.data.position import Position
 from packutils.eval.packing_evaluation import PackingEvaluation, PackingEvaluationWeights
+from packutils.visual.packing_visualization import PackingVisualization
 
-class TestGreedyPacker(unittest.TestCase):
+
+class TestPackingEvaluation(unittest.TestCase):
 
     def setUp(self):
-        bin1 = Bin(6,1,6)
-        bin1.packed_items = [
-            Item("il1", width=2, length=1, height=2,position=
-                Position(x=0, y=0, z=0)
-            ),
-            Item("il2", width=2, length=1, height=2,position=
-                Position(x=2, y=0, z=0)
-            ),
-            Item("il3", width=2, length=1, height=2,position=
-                Position(x=4, y=0, z=0)
-            ),
-            Item("il4", width=2, length=1, height=2,position=
-                Position(x=4, y=0, z=0)
-            ),
-            Item("is1", width=1, length=1, height=1,position=
-                Position(x=0, y=0, z=2)
-            ),
-            Item("is2", width=1, length=1, height=1,position=
-                Position(x=1, y=0, z=2)
-            ),
+        self.bin1 = Bin(6, 1, 6)
+        self.bin1.packed_items = [
+            Item("il1", width=2, length=1, height=2, position=Position(x=0, y=0, z=0)
+                 ),
+            Item("il2", width=2, length=1, height=2, position=Position(x=2, y=0, z=0)
+                 ),
+            Item("il3", width=2, length=1, height=2, position=Position(x=4, y=0, z=0)
+                 ),
+            Item("il4", width=2, length=1, height=2, position=Position(x=4, y=0, z=2)
+                 ),
+            Item("is1", width=1, length=1, height=1, position=Position(x=0, y=0, z=2)
+                 ),
+            Item("is2", width=1, length=1, height=1, position=Position(x=1, y=0, z=2)
+                 ),
         ]
 
-        bin2 = Bin(6,1,6)
-        bin2.packed_items = [
-            Item("il1", width=2, length=1, height=2,position=
-                Position(x=0, y=0, z=0)
-            ),
-            Item("il2", width=2, length=1, height=2,position=
-                Position(x=2, y=0, z=0)
-            ),
-            Item("il3", width=2, length=1, height=2,position=
-                Position(x=4, y=0, z=0)
-            ),
-            Item("il4", width=2, length=1, height=2,position=
-                Position(x=4, y=0, z=0)
-            ),
-            Item("is1", width=1, length=1, height=1,position=
-                Position(x=2, y=0, z=2)
-            ),
-            Item("is2", width=1, length=1, height=1,position=
-                Position(x=3, y=0, z=2)
-            ),
+        self.bin2 = Bin(6, 1, 6)
+        self.bin2.packed_items = [
+            Item("il1", width=2, length=1, height=2, position=Position(x=0, y=0, z=0)
+                 ),
+            Item("il2", width=2, length=1, height=2, position=Position(x=2, y=0, z=0)
+                 ),
+            Item("il3", width=2, length=1, height=2, position=Position(x=4, y=0, z=0)
+                 ),
+            Item("il4", width=2, length=1, height=2, position=Position(x=4, y=0, z=2)
+                 ),
+            Item("is1", width=1, length=1, height=1, position=Position(x=2, y=0, z=2)
+                 ),
+            Item("is2", width=1, length=1, height=1, position=Position(x=3, y=0, z=2)
+                 ),
         ]
 
-        bin3 = Bin(6,1,6)
-        bin3.packed_items = [
-            Item("il1", width=2, length=1, height=2,position=
-                Position(x=0, y=0, z=0)
-            ),
-            Item("il2", width=2, length=1, height=2,position=
-                Position(x=0, y=0, z=2)
-            ),
-            Item("il3", width=2, length=1, height=2,position=
-                Position(x=4, y=0, z=0)
-            ),
-            Item("il4", width=2, length=1, height=2,position=
-                Position(x=4, y=0, z=0)
-            ),
-            Item("is1", width=1, length=1, height=1,position=
-                Position(x=2, y=0, z=0)
-            ),
-            Item("is2", width=1, length=1, height=1,position=
-                Position(x=3, y=0, z=0)
-            ),
+        self.bin3 = Bin(6, 1, 6)
+        self.bin3.packed_items = [
+            Item("il1", width=2, length=1, height=2, position=Position(x=0, y=0, z=0)
+                 ),
+            Item("il2", width=2, length=1, height=2, position=Position(x=0, y=0, z=2)
+                 ),
+            Item("il3", width=2, length=1, height=2, position=Position(x=4, y=0, z=0)
+                 ),
+            Item("il4", width=2, length=1, height=2, position=Position(x=4, y=0, z=2)
+                 ),
+            Item("is1", width=1, length=1, height=1, position=Position(x=2, y=0, z=0)
+                 ),
+            Item("is2", width=1, length=1, height=1, position=Position(x=3, y=0, z=0)
+                 ),
         ]
 
-        
-        bin4 = Bin(6,1,6)
-        bin4.packed_items = [
-            Item("il1", width=2, length=1, height=2,position=
-                Position(x=0, y=0, z=0)
-            ),
-            Item("il2", width=2, length=1, height=2,position=
-                Position(x=0, y=0, z=2)
-            ),
-            Item("il3", width=2, length=1, height=2,position=
-                Position(x=2, y=0, z=0)
-            ),
-            Item("il4", width=2, length=1, height=2,position=
-                Position(x=2, y=0, z=2)
-            ),
-            Item("is1", width=1, length=1, height=1,position=
-                Position(x=4, y=0, z=0)
-            ),
-            Item("is2", width=1, length=1, height=1,position=
-                Position(x=5, y=0, z=0)
-            ),
+        self.bin4 = Bin(6, 1, 6)
+        self.bin4.packed_items = [
+            Item("il1", width=2, length=1, height=2, position=Position(x=0, y=0, z=0)
+                 ),
+            Item("il2", width=2, length=1, height=2, position=Position(x=0, y=0, z=2)
+                 ),
+            Item("il3", width=2, length=1, height=2, position=Position(x=2, y=0, z=0)
+                 ),
+            Item("il4", width=2, length=1, height=2, position=Position(x=2, y=0, z=2)
+                 ),
+            Item("is1", width=1, length=1, height=1, position=Position(x=4, y=0, z=0)
+                 ),
+            Item("is2", width=1, length=1, height=1, position=Position(x=5, y=0, z=0)
+                 ),
         ]
 
     def test_evaluate_item_distribution(self):
@@ -108,26 +85,37 @@ class TestGreedyPacker(unittest.TestCase):
 
         self.assertTrue(score3 > score1 > score2 > score4)
 
-        
     def test_evaluate_item_stacking(self):
+
         eval = PackingEvaluation(PackingEvaluationWeights(
-            item_distribution=1.0
+            item_stacking=1.0
         ))
         score1 = eval.evaluate_bin(self.bin1)
         score2 = eval.evaluate_bin(self.bin2)
         score3 = eval.evaluate_bin(self.bin3)
         score4 = eval.evaluate_bin(self.bin4)
 
-        self.assertTrue(score3 > score1 > score2 > score4)
+        self.assertTrue(score1 == score2 > score3 == score4)
 
-        
     def test_evaluate_item_grouping(self):
+
         eval = PackingEvaluation(PackingEvaluationWeights(
-            item_distribution=1.0
+            item_grouping=1.0
         ))
         score1 = eval.evaluate_bin(self.bin1)
         score2 = eval.evaluate_bin(self.bin2)
         score3 = eval.evaluate_bin(self.bin3)
         score4 = eval.evaluate_bin(self.bin4)
 
-        self.assertTrue(score4 > score3 > score1 > score2)
+        print(score1, score2, score3, score4)
+
+        PackingVisualization().visualize_bin(self.bin1)
+        PackingVisualization().visualize_bin(self.bin2)
+        PackingVisualization().visualize_bin(self.bin3)
+        PackingVisualization().visualize_bin(self.bin4)
+
+        self.assertTrue(score4 > score1 == score2 > score3)
+
+
+if __name__ == '__main__':
+    unittest.main()
