b883144fe90ac75326b02ae76f8b234ecd0452d9
ArnoSchiller <arno-schiller@hotmail.de>
2024-01-26 13:40:16 +0100

    Updated documentation

--- api_packing/api.py
+++ api_packing/api.py
@@ -2,7 +2,27 @@ from fastapi import FastAPI
 
 from v1.api import api_v1
 
+tags_metadata = [
+    {
+        "name": "v1",
+        "description": "API version 1, visit /api/v1/docs for documentation.",
+        "externalDocs": {
+            "description": "Documentation",
+            "url": "http://127.0.0.1/api/v1/docs",
+        },
+    },
+]
 
-app = FastAPI(root_path="/api")
+app = FastAPI(root_path="/api", openapi_tags=tags_metadata)
 
 app.mount("/v1", api_v1)
+
+api_v2 = FastAPI()
+
+
+@api_v2.get("/users")
+def get_users():
+    return {"user": "you-know-who"}
+
+
+app.mount("/v2", api_v2)
--- api_packing/v1/models/variants_request_model.py
+++ api_packing/v1/models/variants_request_model.py
@@ -1,33 +1,49 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 
 from packutils.data.packer_configuration import PackerConfiguration
 
 
 class ArticleModel(BaseModel):
-    id: str
-    width: int
-    length: int
-    height: int
-    amount: int
+    """Article to be packed."""
+
+    id: str = Field(description="Article ID")
+    width: int = Field(description="Article width", gt=0)
+    length: int = Field(description="Article length", gt=0)
+    height: int = Field(description="Article height", gt=0)
+    amount: int = Field(description="Amount of articles", gt=0)
 
 
 class ColliDetailsModel(BaseModel):
-    width: int
-    length: int
-    height: int
-    max_collis: int
-    max_weight: Optional[int] = None
+    """Details of the bins to be packed."""
+
+    width: int = Field(description="Width of the bins to be packed", gt=0)
+    length: int = Field(description="Length of the bins to be packed", gt=0)
+    height: int = Field(description="Height of the bins to be packed", gt=0)
+    max_collis: int = Field(description="Maximum number of bins to be packed", gt=0)
+    max_weight: Optional[int] = Field(
+        description="Maximum weight of the bins to be packed", gt=0, default=None
+    )
 
 
 class OrderModel(BaseModel):
-    order_id: str
-    articles: List[ArticleModel]
-    colli_details: Optional[ColliDetailsModel] = None
+    """Order to be packed."""
+
+    order_id: str = Field(description="Order ID")
+    articles: List[ArticleModel] = Field(description="Articles to be packed")
+    colli_details: Optional[ColliDetailsModel] = Field(
+        description="Details of the bins to be packed", default=None
+    )
     # supplies: List[Supply] = []
 
 
 class VariantsRequestModel(BaseModel):
-    order: OrderModel
-    num_variants: Optional[int]
-    config: Optional[PackerConfiguration]
+    """Request model for packing variants."""
+
+    order: OrderModel = Field(description="Order to be packed")
+    num_variants: Optional[int] = Field(
+        description="Number of packing variants to be generated", gt=0, default=None
+    )
+    config: Optional[PackerConfiguration] = Field(
+        description="Configuration for the packing algorithm", default=None
+    )
