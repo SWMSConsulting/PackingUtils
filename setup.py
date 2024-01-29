d8c6bbb072f583dab420144b25b30aeba0290889
ArnoSchiller <arno-schiller@hotmail.de>
2024-01-29 17:12:56 +0100

    changed package installation

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="packutils",
     version="0.0.1",
     install_requires=[],
+    packages=find_packages(
+        where="packutils", 
+        include=["packutils.*"]
+    ),
 )
