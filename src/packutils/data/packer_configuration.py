273c2e268a13d4d715d9abcd549e73f01c2c5ccd
ArnoSchiller <arno-schiller@hotmail.de>
2024-02-01 13:26:10 +0100

    Marked configuration as pydantic fields

--- src/packutils/data/packer_configuration.py
+++ src/packutils/data/packer_configuration.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class ExtendedEnum(Enum):
@@ -21,28 +21,50 @@ class ItemSelectStrategy(str, ExtendedEnum):
 
 
 class PackerConfiguration(BaseModel):
-    default_select_strategy: Optional[
-        ItemSelectStrategy
-    ] = ItemSelectStrategy.LARGEST_H_W_L
+    default_select_strategy: Optional[ItemSelectStrategy] = Field(
+        description="Default strategy to select items",
+        default=ItemSelectStrategy.LARGEST_L_H_W,
+    )
 
-    new_layer_select_strategy: Optional[
-        ItemSelectStrategy
-    ] = ItemSelectStrategy.LARGEST_H_W_L
+    new_layer_select_strategy: Optional[ItemSelectStrategy] = Field(
+        description="Strategy to select items when starting a new layer",
+        default=ItemSelectStrategy.LARGEST_L_H_W,
+    )
 
-    direction_change_min_volume: Optional[float] = 1.0
+    direction_change_min_volume: Optional[float] = Field(
+        description="Minimum volume of the item to consider changing direction",
+        default=1.0,
+        ge=0.0,
+        le=1.0,
+    )
 
-    bin_stability_factor: Optional[float] = 1.0
+    bin_stability_factor: Optional[float] = Field(
+        description="Stability factor of the bin", default=1.0, ge=0.0, le=1.0
+    )
 
-    allow_item_exceeds_layer: Optional[bool] = False
+    allow_item_exceeds_layer: Optional[bool] = Field(
+        description="Allow items to exceed the current layer height", default=False
+    )
 
-    mirror_walls: Optional[bool] = False
+    mirror_walls: Optional[bool] = Field(
+        description="Place two same items on the outher positions of a new layer",
+        default=False,
+    )
 
-    # padding along the x-axis (width)
-    padding_x: Optional[int] = 0
+    padding_x: Optional[int] = Field(
+        description="Padding between items along the x-axis (width)", default=0, ge=0
+    )
 
-    overhang_y_stability_factor: Optional[float] = None
+    overhang_y_stability_factor: Optional["float|None"] = Field(
+        description="Fraction of the item's length that needs to lay on the bin to be considered stable",
+        default=None,
+        ge=0.55,
+        le=1.0,
+    )
 
-    remove_gaps: Optional[bool] = False
+    remove_gaps: Optional[bool] = Field(
+        description="Remove gaps in x-direction between item stacks", default=False
+    )
 
     def __hash__(self):
         return hash(
@@ -55,5 +77,6 @@ class PackerConfiguration(BaseModel):
                 self.mirror_walls,
                 self.padding_x,
                 self.overhang_y_stability_factor,
+                self.remove_gaps,
             )
         )
